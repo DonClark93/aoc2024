a09c795a347ddb38a670019fb83867092dec5d11
Don Clark <123608546+DonClark93@users.noreply.github.com>
2024-12-03 21:39:25 -0500

    rebuilding to testing2

---
+++ src/Testing2/Testing2.java
@@ -0,0 +1,16 @@
+package Testing2;
+
+public class Testing2 {
+
+    int x = 5;
+
+    public Testing2(){
+    }
+
+    public static void main(String[] args){
+        Testing2 MyObj = new Testing2();
+        System.out.println(MyObj.x);
+        System.out.println("hi");
+    }
+
+}
