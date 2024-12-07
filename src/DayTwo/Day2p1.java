f09dfebdeb72c44be6ebe712484938bdad91a32a
Donald Clark <123608546+DonClark93@users.noreply.github.com>
2024-12-07 15:55:15 -0500

    starting daytwo

---
+++ src/DayTwo/Day2p1.java
@@ -0,0 +1,13 @@
+package DayTwo;
+import java.io.File;
+import java.util.Scanner;
+
+public class Day2p1 {
+
+    static File data = new File("files/DayTwo/data.txt");
+
+    public static void main(String[] args){
+
+    }
+
+}
